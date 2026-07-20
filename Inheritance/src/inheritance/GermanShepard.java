f793b400d6e9d092b9be8f36d06999b0c1d68acd
Akhilesh <akhileshgollangi457@gmail.com>
2026-07-20 09:25:44 +0530

    Inheritances concepts

--- Inheritance/src/inheritance/GermanShepard.java
+++ Inheritance/src/inheritance/GermanShepard.java
@@ -1,4 +1,4 @@
-package com;
+package inheritance;
 
 public class GermanShepard extends Dog{
 }
--- Inheritance/src/inheritance/Main.java
+++ Inheritance/src/inheritance/Main.java
@@ -1,4 +1,4 @@
-package com;
+package inheritance;
 
 public class Main {
     public static void main(String[] args) {
