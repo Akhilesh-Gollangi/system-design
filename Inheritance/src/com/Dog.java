80e8fc710c208918b4f2ace099b3a0dce38f2ce3
Akhilesh <akhileshgollangi457@gmail.com>
2026-07-20 09:21:58 +0530

    Inheritances concepts

---
+++ Inheritance/src/com/Dog.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Dog {
+
+    public void doBark() {
+        System.out.println("Bow Bow");
+    }
+}
---
+++ Inheritance/src/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.doBark(); // parent class method is called here
+
+        GermanShepard germanShepard = new GermanShepard();
+        germanShepard.doBark(); // child class method is called here which got extend from parent class
+        //child class is empty but as it extends it has methods
+        // but during debugging it will go to parent as idea cannot show as it extend thing in child.
+
+    }
+}
