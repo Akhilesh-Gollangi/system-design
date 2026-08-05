6fa53659d3ba46449a39a65e480df513ff311006
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-05 22:41:13 +0530

    LIS fixed solid code

---
+++ lld-hld-system-design/lld/solid-principles/src/com/solid/solidcode_mainak_goscsh/Circle.java
@@ -0,0 +1,17 @@
+package com.solid.solidcode_mainak_goscsh;
+
+// LSP Fixed - Circle only implements Shape2D
+// No longer forced to implement getVolume
+public class Circle implements Shape2D {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
