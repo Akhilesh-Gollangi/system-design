f7d7c61cd1e3b63135756b4ba19a511b69662178
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-10 23:08:39 +0530

    code related to builder design pattern concepts

--- lld-hld-system-design/lld/design-patterns/src/com/builder_design_pattern/concept/notes/withbuilder/Main.java
+++ lld-hld-system-design/lld/design-patterns/src/com/builder_design_pattern/concept/notes/withbuilder/Main.java
@@ -1,16 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+package com.builder_design_pattern.concept.notes.withbuilder;
 
-    UserProfile u1 = new UserProfile.Builder()
-            .name("Mainak")
-            .age(28)
-            .email("mainakgh1@gmail.com")
-            .phoneNumber("123456789")
-            .address("test1")
-            .city("test2")
-            .isVerified(true)
-            .isSubscribedToNewsletter(false)
-            .state("UP").build();
+public class Main {
 
+    public static void main(String[] args) {
+        UserProfile u1 = new UserProfile.Builder()
+                .name("Mainak")
+                .age(28)
+                .email("mainakgh1@gmail.com")
+                .phoneNumber("123456789")
+                .address("test1")
+                .city("test2")
+                .isVerified(true)
+                .isSubscribedToNewsletter(false)
+                .state("UP").build();
+    }
 }
--- lld-hld-system-design/lld/design-patterns/src/com/builder_design_pattern/concept/notes/withoutbuilder/Main.java
+++ lld-hld-system-design/lld/design-patterns/src/com/builder_design_pattern/concept/notes/withoutbuilder/Main.java
@@ -1,45 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    UserProfile u1 = new UserProfile("Mainak",
-            "mainakgh1@gmail.com",
-            28, "9810101010",
-            "test",
-            "delhi",
-            "UP",
-            true,
-            true); // pain point 1 : keep checking the actual class for the order of items
+package com.builder_design_pattern.concept.notes.withoutbuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserProfile u1 = new UserProfile("Mainak",
+                "mainakgh1@gmail.com",
+                28, "9810101010",
+                "test",
+                "delhi",
+                "UP",
+                true,
+                true); // pain point 1 : keep checking the actual class for the order of items
 
 
-    UserProfile u2 = new UserProfile("Sahas",
-            "mainakgh1@gmail.com",
-            28, "9810101010",
-            "test",
-            "delhi",
-            "UP",
-            true,
-            true);
+        UserProfile u2 = new UserProfile("Sahas",
+                "mainakgh1@gmail.com",
+                28, "9810101010",
+                "test",
+                "delhi",
+                "UP",
+                true,
+                true);
 
 
-    UserProfile u3 = new UserProfile("Amruta",
-            "mainakgh1@gmail.com",
-            28, "9810101010",
-            "test",
-            "delhi",
-            "UP",
-            true,
-            true);
+        UserProfile u3 = new UserProfile("Amruta",
+                "mainakgh1@gmail.com",
+                28, "9810101010",
+                "test",
+                "delhi",
+                "UP",
+                true,
+                true);
 
 
-    UserProfile u4 = new UserProfile("Kotari",
-            "mainakgh1@gmail.com",
-            28, "9810101010",
-            "test",
-            "delhi",
-            "UP",
-            true,
-            true); // pain point 2 : if the order of the params in the constructor change, then if you have to fix it everywhere
+        UserProfile u4 = new UserProfile("Kotari",
+                "mainakgh1@gmail.com",
+                28, "9810101010",
+                "test",
+                "delhi",
+                "UP",
+                true,
+                true); // pain point 2 : if the order of the params in the constructor change, then if you have to fix it everywhere
 
-
-
+    }
 }
