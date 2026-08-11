b0032facc71c72ca11bba885a399b17204b04660
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-11 08:12:22 +0530

    code related to state design pattern concepts

---
+++ lld-hld-system-design/lld/design-patterns/src/com/state_design_pattern/practice/withstatedp/Pause.java
@@ -0,0 +1,18 @@
+package com.state_design_pattern.practice.withstatedp;
+
+public class Pause implements IState{
+    @Override
+    public void play(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void pause(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void stop(MediaPlayer mediaPlayer) {
+
+    }
+}
