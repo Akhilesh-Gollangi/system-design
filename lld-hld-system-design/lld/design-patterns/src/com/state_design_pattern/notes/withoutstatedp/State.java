8a60a9a0d758d084e523334813806092fc098261
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-11 08:26:26 +0530

    code related to state design pattern concepts

---
+++ lld-hld-system-design/lld/design-patterns/src/com/state_design_pattern/notes/withoutstatedp/State.java
@@ -0,0 +1,7 @@
+package com.state_design_pattern.notes.withoutstatedp;
+
+public enum State {
+    PLAYING,
+    PAUSED,
+    STOPPED
+}
---
+++ lld-hld-system-design/lld/design-patterns/src/com/state_design_pattern/notes/withstatedp/Main.java
@@ -0,0 +1,22 @@
+package com.state_design_pattern.notes.withstatedp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MediaPlayer player = new MediaPlayer();
+        player.play();
+        player.stop();
+        System.out.println(player.getState());
+
+        player.play();
+        player.pause();
+        System.out.println(player.getState());
+
+
+        player.play();
+        System.out.println(player.getState());
+
+        player.stop();
+        System.out.println(player.getState());
+    }
+}
