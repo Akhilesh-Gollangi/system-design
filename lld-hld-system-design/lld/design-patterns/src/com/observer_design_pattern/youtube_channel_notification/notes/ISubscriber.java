6601a6a06581f329ab13841ac0a8f4d78513ec94
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-13 09:43:22 +0530

    code related to observer design pattern concepts

---
+++ lld-hld-system-design/lld/design-patterns/src/com/observer_design_pattern/youtube_channel_notification/notes/ISubscriber.java
@@ -0,0 +1,6 @@
+package com.observer_design_pattern.youtube_channel_notification.notes;
+
+public interface ISubscriber {
+
+    void notifyUser(String message);
+}
---
+++ lld-hld-system-design/lld/design-patterns/src/com/observer_design_pattern/youtube_channel_notification/notes/YouTubeChannel.java
@@ -0,0 +1,48 @@
+package com.observer_design_pattern.youtube_channel_notification.notes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Flow;
+
+public class YouTubeChannel implements ISubject{
+
+
+    private String channel;
+
+    private List<ISubscriber> subscribers;
+
+    public YouTubeChannel(String channel) {
+        this.channel = channel;
+        subscribers = new ArrayList<>();
+    }
+
+
+    @Override
+    public void subscribe(ISubscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    @Override
+    public void unsubscribe(ISubscriber subscriber) {
+        subscribers.remove(subscriber);
+    }
+
+    // events
+
+    public void uploadVideo(String videoUrl) {
+
+        // some logic related to uploading videos
+        System.out.println("uploading video to " + videoUrl);
+
+        //must notify
+        notifySubscribers("upload completed on " + videoUrl);
+    }
+
+    private void notifySubscribers(String message) {
+        for (ISubscriber subscriber : subscribers) {
+            subscriber.notifyUser(message);
+        }
+    }
+
+
+}
