fc11ce91836d09edb7858889361c0a52ec2cad28
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-14 08:45:44 +0530

    code related to simple factory design pattern concepts

---
+++ lld-hld-system-design/lld/design-patterns/src/com/factor_design_pattern/factory_method/factory_of_levels/Level.java
@@ -0,0 +1,12 @@
+package com.factor_design_pattern.factory_method.factory_of_levels;
+
+public abstract class Level {
+
+    public abstract IEnemy createEnemy();
+
+    public void start() {
+        IEnemy enemy = createEnemy();
+        enemy.attack();
+    }
+
+}
