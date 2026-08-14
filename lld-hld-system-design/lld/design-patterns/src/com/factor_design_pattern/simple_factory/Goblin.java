d4cac54000fd5aedb837c04b96759f8025e5828b
Akhilesh <akhileshgollangi457@gmail.com>
2026-08-14 08:32:41 +0530

    code related to simple factory design pattern concepts

---
+++ lld-hld-system-design/lld/design-patterns/src/com/factor_design_pattern/simple_factory/Goblin.java
@@ -0,0 +1,8 @@
+package com.factor_design_pattern.simple_factory;
+
+public class Goblin implements IEnemy {
+    @Override
+    public void attack() {
+        System.out.println("Goblin attacks with a club!");
+    }
+}
