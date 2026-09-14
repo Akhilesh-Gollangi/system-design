7b92689da35cc55bfb2472546e4280c8ee6dc1f5
Akhilesh <akhileshgollangi457@gmail.com>
2026-09-14 20:24:15 +0530

    creation of thread using extends

--- lld-hld-system-design/lld/multi-threading/src/com/practice/creation_of_threads/extending_thread_class/Main.java
+++ lld-hld-system-design/lld/multi-threading/src/com/practice/creation_of_threads/extending_thread_class/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // here threads are executing concurrently
+        // in single core multiple tasks are running by context switching
+        // but any point of time only thread will execute
+        // if core is idle or after certain time other threads takes core
+        // so everytask make some progress
+        // in sequential if i have 10 independent tasks 10th task has to wait until completion of 9
+        // in concurrent it will started and make some progress
+
+
+        // program : set of instruction, java code
+        // process : java code which is executing in CPU
+        // thread : Path of execution, who will take process to CPU, thread will do that
+        // A process have multiple threads, threads share same resource
+        // Java threads are nothing but normal threads OS, Java provide a way to create Threads
+
+
         //We are printing this using main thread which is default thread
         System.out.println("Entered into class " + Thread.currentThread().getName());
 
@@ -31,6 +47,8 @@ public class Main {
         //thread will execute, when os assign it to core,
         // os will assign randomly, we can give prirority
         // still os will decide which one execute
+
+
 
         for (int i = 0; i < 1000; i++) {
             System.out.println("Hello World");
