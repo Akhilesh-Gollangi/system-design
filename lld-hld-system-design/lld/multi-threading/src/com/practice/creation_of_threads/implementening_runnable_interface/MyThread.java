188eb7f2f51335d20174eeb19e224e690d575808
Akhilesh <akhileshgollangi457@gmail.com>
2026-09-14 22:36:42 +0530

    creation of thread using runnable interface

---
+++ lld-hld-system-design/lld/multi-threading/src/com/practice/creation_of_threads/implementening_runnable_interface/MyThread.java
@@ -0,0 +1,23 @@
+package com.practice.creation_of_threads.implementening_runnable_interface;
+
+public class MyThread implements Runnable {
+
+    private void print() {
+        System.out.println("Task Execution started " + Thread.currentThread().getName());
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i+ " Task Executed by : " + Thread.currentThread().getName());
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Task Finished by "+ Thread.currentThread().getName());
+    }
+
+    @Override
+    public void run() {
+        print();
+    }
+}
